3115cf1ba639efdbc1db6633032cab4c1a14207d
Abubakar <abubkar6287@gmail.com>
2025-10-02 12:38:11 +0530

    Add files via upload

---
+++ CSS-A3.js
@@ -0,0 +1,16 @@
+        gsap.from(".text", {
+            y: 55,
+            opacity: 0,
+            duration: 0.5,
+            stagger: 1,
+            // repeat: 2,
+            
+        }); 
+       
+        gsap.from(".towtext", {
+            x: 80,
+            opacity: 0,
+            duration: 0.5,
+            stagger: 1,
+            repeat: 1,
+        });
